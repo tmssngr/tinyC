a701b25eda42c8ebe7779fbd1e5f05f56fc228f5
Thomas Singer <tmssngr@users.noreply.github.com>
2024-07-21 10:48:00 +0200

    IRWriter

---
+++ src/main/src/com/regnis/tinyc/ir/IRWriter.java
@@ -0,0 +1,85 @@
+package com.regnis.tinyc.ir;
+
+import java.io.*;
+import java.util.*;
+
+import org.jetbrains.annotations.*;
+
+/**
+ * @author Thomas Singer
+ */
+public final class IRWriter {
+
+	private final BufferedWriter writer;
+
+	public IRWriter(@NotNull BufferedWriter writer) {
+		this.writer = writer;
+	}
+
+	public void write(IRProgram program) throws IOException {
+		writeFunctions(program);
+		writeGlobalVars(program.globalVars());
+		writeStringLiterals(program.stringLiterals());
+	}
+
+	private void writeFunctions(IRProgram program) throws IOException {
+		for (IRFunction function : program.functions()) {
+			writeFunction(function);
+		}
+	}
+
+	private void writeFunction(IRFunction function) throws IOException {
+		writeln(function.label() + ":");
+		final List<IRLocalVar> localVars = function.localVars();
+		if (localVars.size() > 0) {
+			writeln(" Local variables");
+			for (IRLocalVar var : localVars) {
+				writeln("   " + var.toString());
+			}
+		}
+
+		for (IRInstruction instruction : function.instructions()) {
+			if (instruction instanceof IRLabel label) {
+				writeln(label.label() + ":");
+			}
+			else {
+				writer.write("        ");
+				if (instruction instanceof IRComment c) {
+					writeln("; " + c.comment());
+				}
+				else {
+					writeln(instruction.toString());
+				}
+			}
+		}
+		writeln("");
+	}
+
+	private void writeGlobalVars(List<IRGlobalVar> globalVars) throws IOException {
+		if (globalVars.isEmpty()) {
+			return;
+		}
+
+		writeln("Global variables");
+		for (IRGlobalVar var : globalVars) {
+			writeln("  " + var.toString());
+		}
+		writeln("");
+	}
+
+	private void writeStringLiterals(List<IRStringLiteral> stringLiterals) throws IOException {
+		if (stringLiterals.isEmpty()) {
+			return;
+		}
+
+		writeln("String literals");
+		for (IRStringLiteral literal : stringLiterals) {
+			writeln("  " + literal.toString());
+		}
+	}
+
+	private void writeln(String text) throws IOException {
+		writer.write(text);
+		writer.newLine();
+	}
+}
