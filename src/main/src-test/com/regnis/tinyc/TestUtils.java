ce02c6503d7310a0d95cb4314dfc7a12531295c7
Thomas Singer <tmssngr@users.noreply.github.com>
2024-08-20 13:03:52 +0200

    TestUtils.assertEquals(List, List): don't check the size, but iterate until one or both are null

--- src/main/src-test/com/regnis/tinyc/TestUtils.java
+++ src/main/src-test/com/regnis/tinyc/TestUtils.java
@@ -10,12 +10,17 @@ import org.junit.*;
  */
 public class TestUtils {
 	public static <E> void assertEquals(List<E> expectedList, List<E> actualList, BiConsumer<E, E> consumer) {
-		Assert.assertEquals(expectedList.size(), actualList.size());
 		final Iterator<E> expIt = expectedList.iterator();
 		final Iterator<E> actualIt = actualList.iterator();
 		while (true) {
-			final E expected = expIt.hasNext() ? expIt.next() : null;
-			final E actual = actualIt.hasNext() ? actualIt.next() : null;
+			final boolean expHasNext = expIt.hasNext();
+			final boolean actualHasNext = actualIt.hasNext();
+			if (!expHasNext && !actualHasNext) {
+				break;
+			}
+
+			final E expected = expHasNext ? expIt.next() : null;
+			final E actual = actualHasNext ? actualIt.next() : null;
 			if (expected == null || actual == null) {
 				Assert.assertEquals(expected, actual);
 				return;
