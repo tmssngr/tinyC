863ef1cf363abc4ee37664858da0241d99e78ad3
Thomas Singer <tmssngr@users.noreply.github.com>
2024-09-15 10:35:47 +0200

    introduce AsmOut

--- src/main/src/com/regnis/tinyc/X86Win64.java
+++ src/main/src/com/regnis/tinyc/X86Win64.java
@@ -12,22 +12,19 @@ import org.jetbrains.annotations.*;
 /**
  * @author Thomas Singer
  */
-public final class X86Win64 {
+public final class X86Win64 extends AsmOut {
 
-	private static final String INDENTATION = "        ";
 	private static final int TMP_REG = 0;
 
-	private final BufferedWriter writer;
-
-	@SuppressWarnings("unused") private boolean debug;
 	private int[] localVarOffsets = new int[0];
 	private int rspOffset;
 
 	public X86Win64(@NotNull BufferedWriter writer) {
-		this.writer = writer;
+		super(writer);
 	}
 
-	public void write(IRProgram program) throws IOException {
+	@Override
+	public void write(@NotNull IRProgram program) throws IOException {
 		writePreample();
 
 		boolean addEmptyLine = false;
@@ -154,9 +151,7 @@ public final class X86Win64 {
 		}
 
 		writeLabel(function.label());
-		for (String line : asmLines) {
-			writeLines(line, line.contains(":") ? "" : INDENTATION);
-		}
+		writeAsm(asmLines);
 	}
 
 	private int prepareLocalVarsOffsets(List<IRLocalVar> localVars) {
@@ -639,45 +634,6 @@ public final class X86Win64 {
 
 	private void writeJump(IRJump jump) throws IOException {
 		writeIndented("jmp " + jump.label());
-	}
-
-	private void writeLabel(String label) throws IOException {
-		write(label + ":");
-		writeNL();
-	}
-
-	private void writeComment(String s) throws IOException {
-		writeIndented("; " + s);
-	}
-
-	private void writeIndented(String text) throws IOException {
-		writeLines(text, INDENTATION);
-	}
-
-	private void writeLines(String text) throws IOException {
-		writeLines(text, null);
-	}
-
-	private void writeLines(String text, @Nullable String leading) throws IOException {
-		final String[] lines = text.split("\\r?\\n");
-		for (String line : lines) {
-			if (leading != null && line.length() > 0) {
-				write(leading);
-			}
-			write(line);
-			writeNL();
-		}
-	}
-
-	private void writeNL() throws IOException {
-		write(System.lineSeparator());
-	}
-
-	private void write(String text) throws IOException {
-		writer.write(text);
-		if (debug) {
-			System.out.print(text);
-		}
 	}
 
 	private static int alignTo16(int offset) {
