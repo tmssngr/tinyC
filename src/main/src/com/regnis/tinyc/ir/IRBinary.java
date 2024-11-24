8e693cbb7c340e66520257d2257ae0fd018065b1
Thomas Singer <tmssngr@users.noreply.github.com>
2024-11-24 12:00:05 +0100

    Introduce IRCompare as a split-of from IRBinary

--- src/main/src/com/regnis/tinyc/ir/IRBinary.java
+++ src/main/src/com/regnis/tinyc/ir/IRBinary.java
@@ -1,7 +1,6 @@
 package com.regnis.tinyc.ir;
 
 import com.regnis.tinyc.*;
-import com.regnis.tinyc.ast.*;
 
 import java.util.*;
 
@@ -12,14 +11,8 @@ import org.jetbrains.annotations.*;
  */
 public record IRBinary(@NotNull IRVar target, @NotNull Op op, @NotNull IRVar left, @NotNull IRVar right, @NotNull Location location) implements IRInstruction {
 	public IRBinary {
-		if (op.relational) {
-			Utils.assertTrue(Objects.equals(left.type(), right.type()), left.type() + " vs. " + right.type());
-			Utils.assertTrue(Objects.equals(target.type(), Type.BOOL), String.valueOf(target.type()));
-		}
-		else {
-			Utils.assertTrue(Objects.equals(target.type(), left.type()), target.type() + " vs. " + left.type());
-			Utils.assertTrue(Objects.equals(target.type(), right.type()), target.type() + " vs. " + right.type());
-		}
+		Utils.assertTrue(Objects.equals(target.type(), left.type()), target.type() + " vs. " + left.type());
+		Utils.assertTrue(Objects.equals(target.type(), right.type()), target.type() + " vs. " + right.type());
 	}
 
 	@Override
@@ -28,15 +21,8 @@ public record IRBinary(@NotNull IRVar target, @NotNull Op op, @NotNull IRVar lef
 	}
 
 	public enum Op {
-		Add(false), Sub(false), Mul(false), Div(false), Mod(false),
-		ShiftLeft(false), ShiftRight(false),
-		And(false), Or(false), Xor(false),
-		Lt(true), LtEq(true), Equals(true), NotEquals(true), GtEq(true), Gt(true);
-
-		private final boolean relational;
-
-		Op(boolean relational) {
-			this.relational = relational;
-		}
+		Add, Sub, Mul, Div, Mod,
+		ShiftLeft, ShiftRight,
+		And, Or, Xor,
 	}
 }
