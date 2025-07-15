9d215ff9f2c6dd70f9896a167de5ec63901e60b4
Thomas Singer <tmssngr@users.noreply.github.com>
2025-07-15 15:03:41 +0200

    TargetArchitecture

--- src/main/src/com/regnis/tinyc/linearscanregalloc/LSArchitecture.java
+++ src/main/src/com/regnis/tinyc/linearscanregalloc/LSArchitecture.java
@@ -1,6 +1,5 @@
 package com.regnis.tinyc.linearscanregalloc;
 
-import com.regnis.tinyc.*;
 import com.regnis.tinyc.ast.*;
 
 import java.util.*;
@@ -10,22 +9,38 @@ import org.jetbrains.annotations.*;
 /**
  * @author Thomas Singer
  */
-public record LSArchitecture(int argRegisterCount, int otherVolatileRegisterCount, int nonVolatileRegisterCount, boolean isX86) implements LSCallingConventionProvider {
+public interface LSArchitecture extends LSCallingConventionProvider {
 
-	public static final LSArchitecture WIN_X86_64 = new LSArchitecture(4, 1, 2, true);
+	int registerCount();
 
-	public LSArchitecture {
-		Utils.assertTrue(argRegisterCount > 0);
-		Utils.assertTrue(otherVolatileRegisterCount >= 0);
-		Utils.assertTrue(nonVolatileRegisterCount >= 0);
-	}
+	boolean isX86();
 
-	@Override
-	public LSCallingConvention getCallingConvention(@NotNull Type targetType, @NotNull List<Type> argTypes) {
-		return LSCallingConvention.createX86CallingConvention(argRegisterCount, otherVolatileRegisterCount);
-	}
+	class Win_X86_64 implements LSArchitecture {
+		private final int argRegisterCount;
+		private final int otherVolatileRegisterCount;
+		private final int nonVolatileRegisterCount;
+		private final LSCallingConvention callingConvention;
 
-	public int registerCount() {
-		return 1 + argRegisterCount + otherVolatileRegisterCount + nonVolatileRegisterCount;
+		public Win_X86_64(int argRegisterCount, int otherVolatileRegisterCount, int nonVolatileRegisterCount) {
+			this.argRegisterCount = argRegisterCount;
+			this.otherVolatileRegisterCount = otherVolatileRegisterCount;
+			this.nonVolatileRegisterCount = nonVolatileRegisterCount;
+			this.callingConvention = LSCallingConvention.createX86CallingConvention(argRegisterCount, otherVolatileRegisterCount);
+		}
+
+		@Override
+		public int registerCount() {
+			return 1 + argRegisterCount + otherVolatileRegisterCount + nonVolatileRegisterCount;
+		}
+
+		@Override
+		public boolean isX86() {
+			return true;
+		}
+
+		@Override
+		public LSCallingConvention getCallingConvention(@NotNull Type targetType, @NotNull List<Type> argTypes) {
+			return callingConvention;
+		}
 	}
 }
