5ac60f6eb819813037d43ce31e0698becdae2fd8
Thomas Singer <tmssngr@users.noreply.github.com>
2025-08-16 18:31:01 +0200

    test unused args

--- src/main/resources-test/basics.c
+++ src/main/resources-test/basics.c
@@ -5,7 +5,13 @@ u8 one = '1';
 u8 two = '2';
 u8 threeFour = 34;
 
+i64 unusedArgs(i16 a, bool b, i64 c, i64 d) {
+	return c;
+}
+
 void main() {
+	i64 c = unusedArgs(1, true, 2, 3);
+
 	printChar(zero);
 
 	u8* onePtr = &one;
