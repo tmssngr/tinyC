6882569e5555906ae548be79e8dee2c61141f952
Thomas Singer <tmssngr@users.noreply.github.com>
2025-08-23 12:17:00 +0200

    test case for unused argument

---
+++ src/main/resources-test/unused.asm
@@ -0,0 +1,78 @@
+format pe64 console
+include 'win64ax.inc'
+
+STD_IN_HANDLE = -10
+STD_OUT_HANDLE = -11
+STD_ERR_HANDLE = -12
+
+entry start
+
+section '.text' code readable executable
+
+start:
+        ; alignment
+        and rsp, -16
+        call init
+        call @main
+        mov rcx, 0
+        sub rsp, 0x20
+        call [ExitProcess]
+
+        ; void unusedArg
+        ;   rsp+16: arg a
+@unusedArg:
+        sub rsp, 8
+        add rsp, 8
+        ret
+
+        ; void main
+@main:
+        sub rsp, 8
+        ; const t.0{r1}, 0
+        mov rcx, 0
+        ; call unusedArg[t.0{r1}]
+        sub rsp, 20h; shadow space
+        call @unusedArg
+        add rsp, 20h
+        add rsp, 8
+        ret
+init:
+        sub rsp, 28h
+          mov rcx, STD_IN_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdIn]
+          mov qword [rcx], rax
+
+          mov rcx, STD_OUT_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdOut]
+          mov qword [rcx], rax
+
+          mov rcx, STD_ERR_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdErr]
+          mov qword [rcx], rax
+        add rsp, 28h
+        ret
+
+section '.data' data readable writeable
+        hStdIn  rb 8
+        hStdOut rb 8
+        hStdErr rb 8
+
+section '.idata' import data readable writeable
+
+library kernel32,'KERNEL32.DLL',\
+        msvcrt,'MSVCRT.DLL'
+
+import kernel32,\
+       ExitProcess,'ExitProcess',\
+       GetStdHandle,'GetStdHandle',\
+       SetConsoleCursorPosition,'SetConsoleCursorPosition',\
+       WriteFile,'WriteFile'
+
+import msvcrt,\
+       _getch,'_getch'
