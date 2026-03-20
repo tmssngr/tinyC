9ffe8891776a17e522c1e20d76ee3f9babb74af9
Thomas Singer <tmssngr@users.noreply.github.com>
2026-03-20 16:59:56 +0100

    AsmWriterTest

---
+++ src/main/resources-test/asmwriter/x86win64/empty.asm
@@ -0,0 +1,60 @@
+format pe64 console
+include 'win64ax.inc'
+
+STD_IN_HANDLE = -10
+STD_OUT_HANDLE = -11
+STD_ERR_HANDLE = -12
+
+entry start
+
+section '.text' code readable executable
+
+start:
+        ; alignment
+        and rsp, -16
+        call init
+        call @main
+        mov rcx, 0
+        sub rsp, 0x20
+        call [ExitProcess]
+
+init:
+        sub rsp, 28h
+          mov rcx, STD_IN_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdIn]
+          mov qword [rcx], rax
+
+          mov rcx, STD_OUT_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdOut]
+          mov qword [rcx], rax
+
+          mov rcx, STD_ERR_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdErr]
+          mov qword [rcx], rax
+        add rsp, 28h
+        ret
+
+section '.data' data readable writeable
+        hStdIn  rb 8
+        hStdOut rb 8
+        hStdErr rb 8
+
+section '.idata' import data readable writeable
+
+library kernel32,'KERNEL32.DLL',\
+        msvcrt,'MSVCRT.DLL'
+
+import kernel32,\
+       ExitProcess,'ExitProcess',\
+       GetStdHandle,'GetStdHandle',\
+       SetConsoleCursorPosition,'SetConsoleCursorPosition',\
+       WriteFile,'WriteFile'
+
+import msvcrt,\
+       _getch,'_getch'
---
+++ src/main/resources-test/asmwriter/x86win64/function.asm
@@ -0,0 +1,81 @@
+format pe64 console
+include 'win64ax.inc'
+
+STD_IN_HANDLE = -10
+STD_OUT_HANDLE = -11
+STD_ERR_HANDLE = -12
+
+entry start
+
+section '.text' code readable executable
+
+start:
+        ; alignment
+        and rsp, -16
+        call init
+        call @main
+        mov rcx, 0
+        sub rsp, 0x20
+        call [ExitProcess]
+
+        ; i64 fn
+        ;   rsp+16: arg a
+        ;   rsp+24: arg b
+        ;   rsp+32: arg c
+        ;   rsp+40: arg d
+        ;   rsp+48: arg e
+        ;   rsp+56: arg f
+        ;   rsp+64: arg g
+        ;   rsp+72: arg temp
+@fn:
+        sub rsp, 8
+        ; const a{r0}, 10
+        mov ax, 10
+        ; const b{r1}, 20
+        mov ecx, 20
+        ; addrof c{r2}, c
+        lea rdx, [rsp+32]
+        ; load d{r3}, [c{r2}]
+        mov r8, [rdx]
+        add rsp, 8
+        ret
+init:
+        sub rsp, 28h
+          mov rcx, STD_IN_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdIn]
+          mov qword [rcx], rax
+
+          mov rcx, STD_OUT_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdOut]
+          mov qword [rcx], rax
+
+          mov rcx, STD_ERR_HANDLE
+          call [GetStdHandle]
+          ; handle in rax, 0 if invalid
+          lea rcx, [hStdErr]
+          mov qword [rcx], rax
+        add rsp, 28h
+        ret
+
+section '.data' data readable writeable
+        hStdIn  rb 8
+        hStdOut rb 8
+        hStdErr rb 8
+
+section '.idata' import data readable writeable
+
+library kernel32,'KERNEL32.DLL',\
+        msvcrt,'MSVCRT.DLL'
+
+import kernel32,\
+       ExitProcess,'ExitProcess',\
+       GetStdHandle,'GetStdHandle',\
+       SetConsoleCursorPosition,'SetConsoleCursorPosition',\
+       WriteFile,'WriteFile'
+
+import msvcrt,\
+       _getch,'_getch'
