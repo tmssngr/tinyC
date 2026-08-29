32edb21ce7ae1224c3b58ed3cb8b4496cf61b984
Thomas Singer <tmssngr@users.noreply.github.com>
2026-08-29 13:55:01 +0200

    IRInstruction.print

--- src/main/src/com/regnis/tinyc/ir/IRInstruction.java
+++ src/main/src/com/regnis/tinyc/ir/IRInstruction.java
@@ -1,7 +1,18 @@
 package com.regnis.tinyc.ir;
 
+import java.util.*;
+
 /**
  * @author Thomas Singer
  */
 public interface IRInstruction {
+
+	static void print(List<IRInstruction> instructions) {
+		for (IRInstruction instruction : instructions) {
+			if (!(instruction instanceof IRLabel)) {
+				System.out.print("        ");
+			}
+			System.out.println(instruction);
+		}
+	}
 }
