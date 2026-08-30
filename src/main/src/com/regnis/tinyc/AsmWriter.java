4aec4388c7edc6dfeb1bbd1f9159e7f5bd7454f9
Thomas Singer <tmssngr@users.noreply.github.com>
2026-08-30 19:21:22 +0200

    AsmWriter/X86*: refactoring

--- src/main/src/com/regnis/tinyc/AsmWriter.java
+++ src/main/src/com/regnis/tinyc/AsmWriter.java
@@ -1,5 +1,6 @@
 package com.regnis.tinyc;
 
+import com.regnis.tinyc.ast.*;
 import com.regnis.tinyc.ir.*;
 
 import java.io.*;
@@ -12,35 +13,39 @@ import org.jetbrains.annotations.*;
  */
 abstract class AsmWriter {
 
-	public abstract void write(@NotNull IRProgram program) throws IOException;
+	protected abstract void writeFunction(IRFunction function) throws IOException;
 
-	protected abstract void writeAddrOf(IRAddrOf addrOf) throws IOException;
+	protected abstract void writeAddrOf(int addrReg, IRVar source) throws IOException;
 
-	protected abstract void writeAddrOfArray(IRAddrOfArray addrOf) throws IOException;
+	protected abstract void writeAddrOfArray(int addrReg, IRVar arrayOrPointer) throws IOException;
 
-	protected abstract void writeBinary(IRBinary binary) throws IOException;
+	protected abstract void writeBinary(IRBinary.Op op, int targetReg, int leftReg, Type leftType, int rightReg, Type rightType) throws IOException;
 
-	protected abstract void writeBranch(IRBranch branch) throws IOException;
+	protected abstract void writeBinary(IRBinary.Op op, int targetReg, int leftReg, Type leftType, int value) throws IOException;
 
-	protected abstract void writeCall(IRCall call) throws IOException;
+	protected abstract void writeBranch(int conditionReg, boolean jumpOnTrue, String targetLabel) throws IOException;
 
-	protected abstract void writeCast(IRCast cast) throws IOException;
+	protected abstract void writeCall(String name) throws IOException;
 
-	protected abstract void writeCompare(IRCompare compare) throws IOException;
+	protected abstract void writeCast(int targetReg, Type targetType, int sourceReg, Type sourceType) throws IOException;
 
-	protected abstract void writeJump(IRJump jump) throws IOException;
+	protected abstract void writeCompare(IRCompare.Op op, int targetReg, int leftReg, int rightReg, Type leftRightType) throws IOException;
 
-	protected abstract void writeMemLoad(IRMemLoad load) throws IOException;
+	protected abstract void writeCompare(IRCompare.Op op, int targetReg, int leftReg, Type leftRightType, int rightValue) throws IOException;
 
-	protected abstract void writeMemStore(IRMemStore store) throws IOException;
+	protected abstract void writeJump(String label) throws IOException;
 
-	protected abstract void writeMove(IRMove copy) throws IOException;
+	protected abstract void writeLiteral(int targetReg, Type type, int value) throws IOException;
 
-	protected abstract void writeRetValue(IRRetValue retValue) throws IOException;
+	protected abstract void writeMemLoad(int targetReg, Type type, int addrReg) throws IOException;
 
-	protected abstract void writeString(IRString literal) throws IOException;
+	protected abstract void writeMemStore(int addrReg, int valueReg, Type valueType) throws IOException;
 
-	protected abstract void writeUnary(IRUnary unary) throws IOException;
+	protected abstract void writeMove(int targetReg, int sourceReg, Type type) throws IOException;
+
+	protected abstract void writeString(int targetReg, Type targetType, int stringLiteralIndex) throws IOException;
+
+	protected abstract void writeUnary(IRUnary.Op op, int targetReg, Type targetType, int sourceReg, Type sourceType) throws IOException;
 
 	private static final String INDENTATION = "        ";
 
@@ -49,6 +54,18 @@ abstract class AsmWriter {
 
 	protected AsmWriter(@NotNull BufferedWriter writer) {
 		this.writer = writer;
+	}
+
+	public void write(@NotNull IRProgram program) throws IOException {
+		for (IRFunction function : program.functions()) {
+			writeNL();
+			writeFunction(function);
+		}
+
+		for (IRAsmFunction function : program.asmFunctions()) {
+			writeNL();
+			writeAsmFunction(function);
+		}
 	}
 
 	protected void writeAsmFunction(IRAsmFunction function) throws IOException {
@@ -74,22 +91,116 @@ abstract class AsmWriter {
 		}
 
 		switch (instruction) {
-		case IRAddrOf addrOf -> writeAddrOf(addrOf);
-		case IRAddrOfArray addrOf -> writeAddrOfArray(addrOf);
-		case IRBinary binary -> writeBinary(binary);
-		case IRBranch branch -> writeBranch(branch);
-		case IRCall call -> writeCall(call);
-		case IRCast cast -> writeCast(cast);
+		case IRAddrOf addrOf -> {
+			final IRVar source = addrOf.source();
+			final IRVar target = addrOf.target();
+			Utils.assertTrue(source.scope() != VariableScope.register);
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			writeAddrOf(target.index(), source);
+		}
+		case IRAddrOfArray addrOf -> {
+			final IRVar arrayOrPointer = addrOf.array();
+			final IRVar addr = addrOf.addr();
+			Utils.assertTrue(arrayOrPointer.scope() != VariableScope.register);
+			Utils.assertTrue(addr.scope() == VariableScope.register);
+			writeAddrOfArray(addr.index(), arrayOrPointer);
+		}
+		case IRBinary binary -> {
+			final IRVar target = binary.target();
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			final IRVar left = binary.left();
+			Utils.assertTrue(left.scope() == VariableScope.register);
+			final IRValue right = binary.right();
+			final IRVar rightVar = right.var();
+			if (rightVar != null) {
+				Utils.assertTrue(rightVar.scope() == VariableScope.register);
+				writeBinary(binary.op(), target.index(), left.index(), left.type(), rightVar.index(), rightVar.type());
+			}
+			else {
+				writeBinary(binary.op(), target.index(), left.index(), left.type(), right.value());
+			}
+		}
+		case IRBranch branch -> {
+			final IRVar conditionVar = branch.conditionVar();
+			Utils.assertTrue(conditionVar.scope() == VariableScope.register);
+			writeBranch(conditionVar.index(), branch.jumpOnTrue(), branch.target());
+		}
+		case IRCall call -> {
+			final IRVar target = call.target();
+			if (target != null) {
+				Utils.assertTrue(target.scope() == VariableScope.register);
+				Utils.assertTrue(target.index() == 0);
+			}
+			writeCall(call.name());
+		}
+		case IRCast cast -> {
+			final IRVar source = cast.source();
+			final IRVar target = cast.target();
+			Utils.assertTrue(source.scope() == VariableScope.register);
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			writeCast(target.index(), target.type(), source.index(), source.type());
+		}
 		case IRComment comment -> writeComment(comment.comment());
-		case IRCompare compare -> writeCompare(compare);
-		case IRJump jump -> writeJump(jump);
+		case IRCompare compare -> {
+			final IRVar target = compare.target();
+			final IRVar left = compare.left();
+			final IRValue right = compare.right();
+			final IRVar rightVar = right.var();
+			if (rightVar != null) {
+				Utils.assertTrue(target.scope() == VariableScope.register);
+				Utils.assertTrue(left.scope() == VariableScope.register);
+				Utils.assertTrue(rightVar.scope() == VariableScope.register);
+				writeCompare(compare.op(), target.index(), left.index(), rightVar.index(), left.type());
+			}
+			else {
+				Utils.assertTrue(target.scope() == VariableScope.register);
+				Utils.assertTrue(left.scope() == VariableScope.register);
+				writeCompare(compare.op(), target.index(), left.index(), left.type(), right.value());
+			}
+		}
+		case IRJump jump -> writeJump(jump.label());
 		case IRLabel label -> writeLabel(label.label());
-		case IRMemLoad load -> writeMemLoad(load);
-		case IRMemStore store -> writeMemStore(store);
-		case IRMove move -> writeMove(move);
-		case IRRetValue retValue -> writeRetValue(retValue);
-		case IRString literal -> writeString(literal);
-		case IRUnary unary -> writeUnary(unary);
+		case IRMemLoad load -> {
+			final IRVar target = load.target();
+			final IRVar addr = load.addr();
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			Utils.assertTrue(addr.scope() == VariableScope.register);
+			writeMemLoad(target.index(), target.type(), addr.index());
+		}
+		case IRMemStore store -> {
+			final IRVar addr = store.addr();
+			final IRVar value = store.value();
+			Utils.assertTrue(addr.scope() == VariableScope.register);
+			Utils.assertTrue(value.scope() == VariableScope.register);
+			writeMemStore(addr.index(), value.index(), value.type());
+		}
+		case IRMove move -> {
+			final IRVar target = move.target();
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			final IRValue source = move.source();
+			final IRVar sourceVar = source.var();
+			if (sourceVar != null) {
+				Utils.assertTrue(sourceVar.scope() == VariableScope.register);
+				Utils.assertTrue(source.type().equals(target.type()));
+				writeMove(target.index(), sourceVar.index(), target.type());
+			}
+			else {
+				writeLiteral(target.index(), target.type(), source.value());
+			}
+		}
+		case IRRetValue ignored -> throw new UnsupportedOperationException("must not be possible");
+		case IRString literal -> {
+			final IRVar target = literal.target();
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			writeString(target.index(), target.type(), literal.stringIndex());
+		}
+		case IRUnary unary -> {
+			final IRVar source = unary.source();
+			final IRVar target = unary.target();
+			Utils.assertTrue(source.scope() == VariableScope.register);
+			Utils.assertTrue(target.scope() == VariableScope.register);
+			writeUnary(unary.op(), target.index(), target.type(), source.index(), source.type());
+		}
 		default -> throw new UnsupportedOperationException(instruction.getClass() + " " + instruction);
 		}
 	}
