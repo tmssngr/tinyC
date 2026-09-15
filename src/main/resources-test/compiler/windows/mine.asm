2fdd099ee20470be6c529d0e89cc5d52067a76c6
Thomas Singer <tmssngr@users.noreply.github.com>
2026-09-15 20:17:17 +0200

    fix win_x86_64.h
    it corrupted rbx

--- src/main/resources-test/compiler/windows/mine.asm
+++ src/main/resources-test/compiler/windows/mine.asm
@@ -2044,7 +2044,8 @@ _printStringLength@@u8@i64:
 
         ; i16 getChar
 _getChar:
-        sub    rsp, 28h
+        push   rbx
+        sub    rsp, 20h
           call [_getch]
           test al, al
           js   .1
@@ -2056,7 +2057,8 @@ _getChar:
           call [_getch]
           or   rax, rbx
 .2:
-        add    rsp, 28h
+        add    rsp, 20h
+        pop    rbx
         ret
 
         ; void setCursor@i16@i16
