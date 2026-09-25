abea06cded4d82a7740f4afa79abf32acd92d5b0
Thomas Singer <tmssngr@users.noreply.github.com>
2026-09-25 13:56:36 +0200

    Refactoring IRVarReplacer

--- src/main/src/com/regnis/tinyc/ir/IRVarReplacer.java
+++ src/main/src/com/regnis/tinyc/ir/IRVarReplacer.java
@@ -16,30 +16,12 @@ public abstract class IRVarReplacer {
 		return switch (instruction) {
 			case IRAddrOf addrOf -> new IRAddrOf(replace(addrOf.target()), replace(addrOf.source()), addrOf.location());
 			case IRAddrOfArray addrOf -> new IRAddrOfArray(replace(addrOf.addr()), replace(addrOf.array()), addrOf.location());
-			case IRBinary binary -> {
-				IRValue right = binary.right();
-				final IRVar rightVar = right.var();
-				if (rightVar != null) {
-					right = new IRValue(replace(rightVar));
-				}
-				yield new IRBinary(replace(binary.target()), binary.op(), replace(binary.left()), right, binary.location());
-			}
-			case IRBranch branch -> {
-				IRValue right = branch.right();
-				final IRVar rightVar = right.var();
-				if (rightVar != null) {
-					right = new IRValue(replace(rightVar));
-				}
-				yield new IRBranch(branch.op(), replace(branch.left()), right, branch.target(), branch.nextLabel(), branch.location());
-			}
+			case IRBinary binary -> new IRBinary(replace(binary.target()), binary.op(), replace(binary.left()), replace(binary.right()), binary.location());
+			case IRBranch branch -> new IRBranch(branch.op(), replace(branch.left()), replace(branch.right()), branch.target(), branch.nextLabel(), branch.location());
 			case IRCall call -> {
 				final List<IRValue> args = new ArrayList<>();
 				for (IRValue arg : call.args()) {
-					final IRVar argVar = arg.var();
-					if (argVar != null) {
-						arg = new IRValue(replace(argVar));
-					}
-					args.add(arg);
+					args.add(replace(arg));
 				}
 				IRVar target = call.target();
 				if (target != null) {
@@ -49,30 +31,25 @@ public abstract class IRVarReplacer {
 			}
 			case IRCast cast -> new IRCast(replace(cast.target()), replace(cast.source()), cast.location());
 			case IRComment ignored -> instruction;
-			case IRCompare compare -> {
-				IRValue right = compare.right();
-				final IRVar rightVar = right.var();
-				if (rightVar != null) {
-					right = new IRValue(replace(rightVar));
-				}
-				yield new IRCompare(replace(compare.target()), compare.op(), replace(compare.left()), right, compare.location());
-			}
+			case IRCompare compare -> new IRCompare(replace(compare.target()), compare.op(), replace(compare.left()), replace(compare.right()), compare.location());
 			case IRLabel ignored -> instruction;
 			case IRJump ignored -> instruction;
 			case IRMemLoad load -> new IRMemLoad(replace(load.target()), replace(load.addr()), load.location());
 			case IRMemStore store -> new IRMemStore(replace(store.addr()), replace(store.value()), store.location());
-			case IRMove move -> {
-				IRValue source = move.source();
-				final IRVar sourceVar = source.var();
-				if (sourceVar != null) {
-					source = new IRValue(replace(sourceVar));
-				}
-				yield new IRMove(replace(move.target()), source, move.location());
-			}
+			case IRMove move -> new IRMove(replace(move.target()), replace(move.source()), move.location());
 			case IRRetValue retValue -> new IRRetValue(replace(retValue.var()), retValue.location());
 			case IRString literal -> new IRString(replace(literal.target()), literal.stringIndex(), literal.location());
 			case IRUnary unary -> new IRUnary(unary.op(), replace(unary.target()), replace(unary.source()));
 			default -> throw new UnsupportedOperationException(String.valueOf(instruction));
 		};
+	}
+
+	@NotNull
+	private IRValue replace(@NotNull IRValue value) {
+		final IRVar var = value.var();
+		if (var != null) {
+			value = new IRValue(replace(var));
+		}
+		return value;
 	}
 }
